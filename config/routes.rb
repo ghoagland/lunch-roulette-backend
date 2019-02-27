8f1e1eaca4e24c3f664a03a6d275d07e906081a4
ghoagland-flatiron <georgina.hoagland@flatironschool.com>
2019-02-27 09:14:56 -0500

    added basic user model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :users
-  resources :emails
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+   namespace :v1 do
+
+   end
+ end
 end
