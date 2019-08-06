86fe33dee8eadce23505dcc6b3d122d24974b5f3
Georgina Hoagland <georgina.hoagland@flatironschool.com>
2019-08-06 11:21:02 -0400

    whitespace

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -13,20 +13,38 @@ class Api::V1::UsersController < ApplicationController
     @user = User.create(user_params)
     if @user.valid?
       @token = encode_token(user_id: @user.id)
-      render json: { user: UserSerializer.new(@user, include: [:user_restaurants]).serializable_hash, jwt: @token }, status: :created
+      render ({
+        json: {
+          user: UserSerializer.new(@user, user_serializer_options),
+          jwt: @token
+        },
+        status: :created
+      })
     else
       render json: { error: 'failed to create user' }, status: :not_acceptable
     end
   end
 
   def profile
-    render json: { user: UserSerializer.new(current_user, include: [:user_restaurants]).serializable_hash }, status: :accepted
+
+    render ({
+      json: {
+        user: UserSerializer.new(@user, user_serializer_options)
+      },
+      status: :accepted
+    })
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :password, :email, :default_location)
+  end
+
+  def user_serializer_options
+    return {
+      include: [:restaurants, :'restaurants.name', :'restaurants.yelp_id']
+    }
   end
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -4,5 +4,5 @@ class Restaurant < ApplicationRecord
 
   has_many :user_restaurants
   has_many :users, through: :user_restaurants
-
+  
 end
